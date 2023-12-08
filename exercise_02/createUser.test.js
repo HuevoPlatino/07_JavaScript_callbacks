de935c6711db38b4821844a9f47ae8342f2c562c
Jonathan del castillo <Jonathan.delcastillo@mataro.epiaedu.cat>
2023-12-08 17:47:53 +0100

    The file has been completely changed

--- exercise_02/createUser.test.js
+++ exercise_02/createUser.test.js
@@ -2,21 +2,37 @@ import createUser from "./createUser.js";
 import onError from "./callbacks/onError.js";
 import onSuccess from "./callbacks/onSuccess.js";
 
-//test with email as empty string and log result
 
-describe("When function is called with a value email",() =>{
-    const email = " ";
 
-    test(`User with email ${email} has been correctly created sholud be returned`,() =>{
-      // Arrange
-      const email = "jona7han@gmail.com";
+describe("Given function createUser",() =>{
+    describe("When function is called with value empty",() =>{
+        test("Then it should return 'Error: user not created'",() =>{
 
-      // Act
-      const result = createUser(email, onError, onSuccess);
+            //Arrange
+            const email = " ";
+            const expected = "Error: user not created";
 
-      // Assert
-      expect(result).toEqual(`User with email ${email} has been correctly created`
-      );
-    })
-}
-)
+            //Act
+            const result = createUser(email, onError, onSuccess);
+            
+            //Assert
+            expect(result).toEqual(expected);
+        });
+    });
+
+describe("When function is called with value string",() =>{
+    test("Then it should return 'User with email {email} has been correctly created'",() =>{
+
+             //Arrange
+              const email = "jona7han@gmail.com";
+              const expected = "User with email jona7han@gmail.com has been correctly created";
+
+              //Act
+              const result = createUser(email, onError, onSuccess);
+            
+             //Assert
+             expect(result).toEqual(expected);    
+        });
+    });
+});
+
